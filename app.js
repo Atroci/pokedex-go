229a186017c6558614a0f2e0f47d144f278666a3
Atroci <hugomoreiradecarvalho@gmail.com>
2016-08-24 18:48:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static('public'));
 
 // base route
 app.get('/', (req, res) => {
-	res.status(200).send('works! 💪👌💯🙌');
+	res.status(200).send('1593488668�');
 });
 
 app.get('/webhook', (req, res) => {
